59ff1c2b8307ea83633707036db9eeb7ea83f049
Philip Schiffrin <philip.schiffrin@gmail.com>
2017-12-07 20:05:26 -0500

    sends message to client on connect

--- ledger.hpp
+++ ledger.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <ctime>
 #include <string>
+#include <iostream>
 #include <boost/asio.hpp>
 #include <boost/bind.hpp>
 #include <boost/shared_ptr.hpp>
@@ -34,14 +35,19 @@ public:
     connections.push_back(tcp::socket(acceptor.get_io_service()));
 
 
-    acceptor.async_accept(connections.back(), boost::bind(&Ledger::acceptHandler, this, boost::asio::placeholders::error));
+    acceptor.async_accept(connections.back(), boost::bind(&Ledger::acceptHandler, this, &connections.back(), boost::asio::placeholders::error));
   }
 
-  void acceptHandler(const boost::system::error_code& e) {
+  void acceptHandler(tcp::socket *socket, const boost::system::error_code& e) {
 
     if (!e) {
     cout << "accepted new connection" << endl;
     }
+
+    std::string hello = "hi";
+
+    boost::asio::write(*socket, boost::asio::buffer(hello));
+    
 
     startAccept();
 
@@ -51,84 +57,6 @@ public:
   
 
 };
-
-std::string make_daytime_string()
-{
-    std:: time_t now = std::time(0);
-      return std::ctime(&now);
-}
-
-
-class tcp_connection
-  // Using shared_ptr and enable_shared_from_this 
-  // because we want to keep the tcp_connection object alive 
-  // as long as there is an operation that refers to it.
-  : public boost::enable_shared_from_this<tcp_connection>
-{
-public:
-  typedef boost::shared_ptr<tcp_connection> pointer;
-
-  static pointer create(boost::asio::io_service& io_service)
-  {
-    return pointer(new tcp_connection(io_service));
-  }
-
-  tcp::socket& socket()
-  {
-    return socket_;
-  }
-
-  // Call boost::asio::async_write() to serve the data to the client. 
-  // We are using boost::asio::async_write(), 
-  // rather than ip::tcp::socket::async_write_some(), 
-  // to ensure that the entire block of data is sent.
-
-  void start()
-  {
-    // The data to be sent is stored in the class member m_message 
-    // as we need to keep the data valid 
-    // until the asynchronous operation is complete.
-    m_message = make_daytime_string();
-
-    // When initiating the asynchronous operation, 
-    // and if using boost::bind(), 
-    // we must specify only the arguments 
-    // that match the handler's parameter list. 
-    // In this code, both of the argument placeholders 
-    // (boost::asio::placeholders::error 
-    // and boost::asio::placeholders::bytes_transferred) 
-    // could potentially have been removed, 
-    // since they are not being used in handle_write().
-
-    boost::asio::async_write(socket_, boost::asio::buffer(m_message),
-        boost::bind(&tcp_connection::handle_write, shared_from_this(),
-          boost::asio::placeholders::error,
-          boost::asio::placeholders::bytes_transferred));
-  }
-
-private:
-  tcp_connection(boost::asio::io_service& io_service)
-    : socket_(io_service)
-  {
-  }
-  // handle_write() is responsible for any further actions 
-  // for this client connection.
-  void handle_write(const boost::system::error_code& /*error*/,
-      size_t /*bytes_transferred*/)
-  {
-  }
-
-  tcp::socket socket_;
-  std::string m_message;
-};
-
-
-
-
-
-
-
-
 
 
 
