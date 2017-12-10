82e9925e9c25d787885e569aa40b32a9d5461e79
Philip Schiffrin <philip.schiffrin@gmail.com>
2017-12-09 20:03:11 -0500

    added tcp_connection.hpp

---
+++ tcp_connection.hpp
@@ -0,0 +1,40 @@
+#ifndef TCP_CONNECTION_H
+#define TCP_CONNECTION_H
+
+#include <boost/asio.hpp>
+#include <boost/bind.hpp>
+#include <boost/shared_ptr.hpp>
+#include <boost/enable_shared_from_this.hpp>
+
+
+using boost::asio::ip::tcp;
+using boost::asio::io_service;
+
+class tcp_connection
+  // Using shared_ptr and enable_shared_from_this 
+  // because we want to keep the tcp_connection object alive 
+  // as long as there is an operation that refers to it.
+  : public boost::enable_shared_from_this<tcp_connection>
+{
+public:
+  typedef boost::shared_ptr<tcp_connection> pointer;
+
+  static pointer create(boost::asio::io_service& io_service)
+  {
+    return pointer(new tcp_connection(io_service));
+  }
+
+  tcp::socket& socket()
+  {
+    return socket_;
+  }
+
+  tcp_connection(boost::asio::io_service& io_service)
+    : socket_(io_service)
+  {
+  }
+
+  tcp::socket socket_;
+};
+
+#endif
