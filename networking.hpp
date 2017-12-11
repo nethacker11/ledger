0d4a4b05bae6432cd57e4e65158e1baecc8e3936
Philip Schiffrin <philip.schiffrin@gmail.com>
2017-12-10 19:12:19 -0500

    cleaning up print statements

--- networking.hpp
+++ networking.hpp
@@ -6,5 +6,4 @@
 const std::string ADDRESS = "127.0.0.1";
 const std::string PORT_STR = "8888";
 
-// delim works with a \n, not sure why, this is a hack
-const std::string DELIM = "\n|||";
+const std::string DELIM = "|||";
